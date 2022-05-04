d48026f2920d5a8df6e2eefafbf8624ec28dbeb3
DeokhunKim <86358502+DeokhunKim@users.noreply.github.com>
2022-05-04 17:07:40 +0900

    Add basic file of spring boot

---
+++ src/main/java/com/example/kppgateway/KppGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.kppgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KppGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KppGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kppgateway/KppGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kppgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KppGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
