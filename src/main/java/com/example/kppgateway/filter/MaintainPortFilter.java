c4525e8918fe22987358a8db33b78419d85b4bcd
DeokhunKim <koon0312@naver.com>
2022-05-28 21:04:19 +0900

    Maintain host

--- src/main/java/com/example/kppgateway/filter/MaintainPortFilter.java
+++ src/main/java/com/example/kppgateway/filter/MaintainPortFilter.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.gateway.filter.factory.SetRequestHostHeaderGate
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.net.InetSocketAddress;
 import java.net.URI;
 
 @Component
@@ -22,13 +23,14 @@ public class MaintainPortFilter extends AbstractGatewayFilterFactory<MaintainPor
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
             try {
-                URI uri =  exchange.getResponse().getHeaders().getLocation();
-                if (uri == null) {
+                URI responseUri =  exchange.getResponse().getHeaders().getLocation();
+                if (responseUri == null) {
                     return;
                 }
+                URI requestUri = exchange.getRequest().getURI();
                 int port = exchange.getRequest().getHeaders().getHost().getPort();
                 exchange.getResponse().getHeaders().setLocation(
-                        new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), port, uri.getPath(), uri.getQuery(), uri.getFragment()));
+                        new URI(responseUri.getScheme(), responseUri.getUserInfo(), requestUri.getHost(), port, responseUri.getPath(), responseUri.getQuery(), responseUri.getFragment()));
             }catch (Exception e) {
                 e.printStackTrace();
             }
